c8eb157ecb05c133b47d81549a22fbf27bfdc879
phucha1986 <hahanhphuc1986@gmail.com>
2025-07-06 22:28:07 +0400

    update site

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-//import manifest from './manifest.json';
+import manifest from './manifest.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      manifest,
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       // switch to "true" to enable sw on development
       devOptions: { enabled: false },
